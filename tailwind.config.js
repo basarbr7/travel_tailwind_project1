262d37e4f4f1900db2f5ae0e85f4028437e6c68e
M.A. Basar <basarbr7@gmail.com>
2024-12-23 14:22:46 +0600

    added selling part

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,36 +3,38 @@ module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
     fontFamily: {
-      'popphins': ["Poppins", "serif"],
-      'Volkhov': ["Volkhov", "serif"],
-      'Open_Sans': ["Open Sans", "serif"],
+      'poppins': ["Poppins", "serif"], // Corrected typo
+      'volkhov': ["Volkhov", "serif"],
+      'openSans': ["Open Sans", "serif"],
     },
     extend: {
-      container: {
-        center: true, // Centers the container
-        padding: {
-          DEFAULT: '1rem', // Default padding (16px)
-          // sm: '2rem',      // Padding for small screens
-          // lg: '4rem',      // Padding for large screens
-          // xl: '5rem',      // Padding for extra-large screens
-          // '2xl': '6rem',   // Padding for 2xl screens
-        },
-        screens: {
-          sm: '640px',  // Small screens
-          md: '768px',  // Medium screens
-          lg: '1024px', // Large screens
-          xl: '1200px', // Extra-large screens
-          '2xl': '1536px', // 2X Extra-large screens
-        },
-      },
+      // container: {
+      //   center: true, // Centers the container
+      //   padding: {
+      //     DEFAULT: '1rem', // Default padding (16px)
+      //     // sm: '2rem',      // Padding for small screens
+      //     // lg: '4rem',      // Padding for large screens
+      //     // xl: '5rem',      // Padding for extra-large screens
+      //     // '2xl': '6rem',   // Padding for 2xl screens
+      //   },
+      //   screens: {
+      //     sm: '640px',  // Small screens
+      //     md: '768px',  // Medium screens
+      //     lg: '1024px', // Large screens
+      //     xl: '1200px', // Extra-large screens
+      //     '2xl': '1536px', // 2X Extra-large screens
+      //   },
+      // },
       spacing: {
         'myContainer': '1200px',
       },
       boxShadow: {
         shadow1: '0px 20px 35px 0px rgba(241,165,1,0.15)',
         playShadow: '0px 15px 30px 0px rgba(223,105,81,1)',
+        serviceShadow: '0px 45px 30px -3px rgba(0,0,0,0.13)',
+        sellingShadow: '0px 100px 80px 0px rgba(0,0,0,0.2)',
       },
     },
   },
   plugins: [],
-}
+};
