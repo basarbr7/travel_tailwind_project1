1174d9c010a90eaeae5a39801b6fbb03eb2c9dff
M.A. Basar <basarbr7@gmail.com>
2024-12-29 22:29:43 +0600

    added footer and more

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,32 +11,29 @@ module.exports = {
       letterSpacing: {
         'custom': '0.015em', // Custom letter-spacing for 1.5%
       },
-      // container: {
-      //   center: true, // Centers the container
-      //   padding: {
-      //     DEFAULT: '1rem', // Default padding (16px)
-      //     // sm: '2rem',      // Padding for small screens
-      //     // lg: '4rem',      // Padding for large screens
-      //     // xl: '5rem',      // Padding for extra-large screens
-      //     // '2xl': '6rem',   // Padding for 2xl screens
-      //   },
-      //   screens: {
-      //     sm: '640px',  // Small screens
-      //     md: '768px',  // Medium screens
-      //     lg: '1024px', // Large screens
-      //     xl: '1200px', // Extra-large screens
-      //     '2xl': '1536px', // 2X Extra-large screens
-      //   },
-      // },
+      backgroundImage: {
+        'conic-custom': `conic-gradient(
+
+        from 180deg,
+        #B8D2F1 0%,
+        #F289AA 17%,
+        #C68BF0 29%,
+        #D164DA 43%,
+        #C963E8 61%,
+        #BFC2E8 72%,
+        #FFC999 80%,
+        #CED8CB 100%,
+        #FFFFFF 100%
+        )`,
+      },
       spacing: {
         'myContainer': '1200px',
       },
       boxShadow: {
         shadow1: '0px 20px 35px 0px rgba(241,165,1,0.15)',
         playShadow: '0px 15px 30px 0px rgba(223,105,81,1)',
-        serviceShadow: '0px 38.9px 29.16px 0px rgba(0,0,0,0.11)',
-        sellingShadow: '0px 38.9px 29.16px 0px rgba(0,0,0,0.11)',
-        tripShadow: '0px 38.9px 29.16px 0px rgba(0,0,0,0.11)',
+        customShadow: '0px 38.9px 29.16px 0px rgba(0,0,0,0.11)',
+        footerShadow: '0px 2px 10px 0px rgba(0,0,0,0.1)',
       },
     },
   },
